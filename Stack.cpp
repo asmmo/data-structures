3b813ba734255e32a073d0b9807656ecae7bae4d
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-05-03 15:12:52 +0200

    Create Stack.cpp
    simple stack using singly linked list

---
+++ Stack.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template<typename T>
+class Stack{
+    struct Node{
+        Node * next{};
+        T data{};
+
+        Node(T data, Node * next = nullptr ) : data{data}, next { next }{
+
+        }
+    };
+
+    Node * head {};
+    size_t _size{};
+
+public:
+    Stack(const Stack & rhs) = delete ;
+    void push_front(T data ){
+        head = new Node { data, head};
+        ++_size;
+    }
+
+    void pop(){
+        if(_size) {
+            Node * ptr = head;
+            head = head -> next;
+            delete ptr;
+            --_size;
+        }
+    }
+
+    T top(){
+        return head -> data;
+    }
+
+    ~Stack(){
+        while( _size ) pop();
+    }
+
+};
